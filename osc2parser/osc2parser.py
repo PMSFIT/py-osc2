9e344cfd7d1704081e7353d83f4e7ddfadc8eb84
Pierre R. Mai <pmai@pmsf.de>
2021-12-18 21:19:50 +0100

    Display parse result as indented tree

--- osc2parser/osc2parser.py
+++ osc2parser/osc2parser.py
@@ -1,5 +1,7 @@
 import sys
+import re
 from antlr4 import *
+from antlr4.tree.Tree import TerminalNodeImpl
 from openscenario2Parser import openscenario2Parser
 from openscenario2Lexer import openscenario2Lexer
  
@@ -9,7 +11,16 @@ def main(argv):
     stream = CommonTokenStream(lexer)
     parser = openscenario2Parser(stream)
     tree = parser.osc_file()
-    print(tree.toStringTree(openscenario2Parser.ruleNames))
- 
+    process(tree, parser.ruleNames)
+
+def process(tree, ruleNames, indent = 0):
+    if isinstance(tree, TerminalNodeImpl):
+        if not re.match(r"\r?\n[ \t]*",tree.getText()):
+            print("{0}TOKEN '{1}'".format("  " * indent, tree.getText()))
+    else:
+        print("{0}{1}".format("  " * indent, ruleNames[tree.getRuleIndex()]))
+        for child in tree.children:
+            process(child,ruleNames,indent+1)
+
 if __name__ == '__main__':
     main(sys.argv)

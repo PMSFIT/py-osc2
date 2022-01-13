7667e92b848da4fb960c19623ebf00c033896573
Pierre R. Mai <pmai@pmsf.de>
2022-01-13 23:41:38 +0100

    Bump version number for 0.0.4

--- osc2parser/__init__.py
+++ osc2parser/__init__.py
@@ -2,7 +2,7 @@
 
 (C) 2021-2022 PMSF IT Consulting Pierre R. Mai
 """
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 from .openscenario2Parser import openscenario2Parser
 from .openscenario2Lexer import openscenario2Lexer
 from .osc2parser import parse_file

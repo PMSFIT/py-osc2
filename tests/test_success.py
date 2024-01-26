d200cf9d83cf24dd0a13f0ce1ae9e144a28cc0d0
Pierre R. Mai <pmai@pmsf.de>
2024-01-26 16:31:15 +0100

    Add testcases for namespaces and float literals

--- tests/test_success.py
+++ tests/test_success.py
@@ -13,5 +13,11 @@ class TestCorrectParse(unittest.TestCase):
     def test_sample_expression(self):
         self.assertEqual(osc2parser.parse_file(example_file('sample_expression.osc'))[1],0)
 
+    def test_namespaces(self):
+        self.assertEqual(osc2parser.parse_file(example_file('namespaces.osc'))[1],0)
+
+    def test_floatliterals(self):
+        self.assertEqual(osc2parser.parse_file(example_file('float_literals.osc'))[1],0)
+
 if __name__ == '__main__':
     unittest.main()
